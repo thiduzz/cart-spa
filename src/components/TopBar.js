5a06b2d171480dcda18b7e38f484adb4f73249f9
Thiago Mello <thiduzz14@gmail.com>
2021-07-31 09:40:30 +0200

    Introduce React Icons

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,13 @@
 // @flow
 import * as React from 'react';
+import {FiShoppingCart} from "react-icons/fi";
 
 export function TopBar(props) {
     return (
-        <div className="bg-gray-400 shadow-md w-full">
+        <div className="bg-white-400 shadow-md w-full">
             <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-                <h1 className="font-fabarie uppercase text-white-400 text-5xl font-extralight">Fabarie</h1>
-                <div>Cart</div>
+                <h1 className="font-fabarie uppercase text-gray-400 text-5xl font-extralight">Fabarie</h1>
+                <div><FiShoppingCart size={32} className="text-gray-400"/></div>
             </div>
         </div>
     );
