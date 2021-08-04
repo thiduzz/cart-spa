55e35cccec150e3223d0a564267670462eca382b
Thiago <thiduzz14@gmail.com>
2021-08-04 08:26:49 +0200

    Add the cart button product count utilizing the cart context

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,14 +1,18 @@
 // @flow
 import * as React from 'react';
+import './CartButton.scss'
 import {FiShoppingCart} from "react-icons/fi";
+import CartContext from "../../store/cart-context";
+import {useContext} from "react";
 
 export function CartButton() {
+    const ctx = useContext(CartContext)
     return (
-        <div className="bg-white-400 px-10 py-2 shadow-inner shadow-2xl rounded-full">
+        <div className="cart-btn">
             <div className="relative">
-                <span className="bg-red-400 rounded-full p-1 font-fabarie w-6 h-6 absolute text-white-400 text-center leading-normal flex items-center justify-center -top-3 -right-4">3</span>
+                <span className="cart-btn-bubble">{ctx.items.length}</span>
                 <FiShoppingCart size={32} className="text-gray-400"/>
             </div>
         </div>
-    );
+    )
 };
