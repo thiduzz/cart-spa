9fa33662045e4c52c3d1c63bf5f782b9ef328582
Thiago <thiduzz14@gmail.com>
2021-08-04 09:34:26 +0200

    Adapt the CartModal with placeholder components

--- src/components/Cart/CartModal.js
+++ src/components/Cart/CartModal.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const CartModal = (props) => {
+    const ctx = useContext(CartContext)
+    const hasItemsInCart = ctx.items.length > 0;
     return (
         <Modal onClose={props.onClose}>
-            Cart Modal
+            {hasItemsInCart && <ul className="w-full">{ctx.items.map((item) => <CartItem item={item} key={item.id}/>)}</ul>}
+            {hasItemsInCart &&  <div>
+                <div>Total amount: X</div>
+                <div>
+                    <button>Close</button>
+                    <button>Order</button>
+                </div>
+            </div>}
+            {!hasItemsInCart && <h1>No items in the cart</h1>}
         </Modal>
     );
 };
