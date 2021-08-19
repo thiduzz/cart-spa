c7be51f3ff4df257087fd7c2a0a8d036050f8ce1
Thiago Mello <thiduzz14@gmail.com>
2021-08-19 09:34:43 +0200

    Adapt the input component to also show a error message

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,23 +3,26 @@ import './Input.scss'
 
 const Input = (props) => {
     const inputType = props.type || 'text';
-    const wrappingClasses = 'relative h-10 input-component mb-5 ' + props.className + (props.value ? '' : ' empty ') + (props.hasError ? ' with-error ' : '')
+    const wrappingClasses = 'relative h-10 input-component ' + (props.className || '') + (props.value ? '' : ' empty ') + (props.hasError ? ' with-error ' : '')
     const name = props.name || props.id;
 
     return (
-        <div className={wrappingClasses}>
-            <input
-                id={props.id}
-                type={inputType}
-                name={name}
-                className="h-full w-full border-gray-300 px-2 transition-all border-blue rounded-sm"
-                value={props.value}
-                onBlur={props.onBlur}
-                onChange={props.onChange}
-            />
-            <label htmlFor={name} className="absolute left-2 transition-all bg-white-100 px-1">
-                {props.label}
-            </label>
+        <div className={(props.className || '') + ' mb-2'}>
+            <div className={wrappingClasses}>
+                <input
+                    id={props.id}
+                    type={inputType}
+                    name={name}
+                    className="h-full w-full border-gray-300 px-2 transition-all border-blue rounded-sm"
+                    value={props.value}
+                    onBlur={props.onBlur}
+                    onChange={props.onChange}
+                />
+                <label htmlFor={name} className="absolute left-2 transition-all bg-white-100 px-1">
+                    {props.label}
+                </label>
+            </div>
+            {props.hasError && <span className="text-red-400 text-xs py-1">{props.error || "invalid_input"}</span>}
         </div>
     );
 };
