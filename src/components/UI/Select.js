6d97f59e6fe3b38f721867bf517bf8126624a529
Thiago Mello <thiduzz14@gmail.com>
2021-08-20 09:33:36 +0200

    Style select component and make label fixed on top

--- src/components/UI/Select.js
+++ src/components/UI/Select.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Select.scss'
 
 const Select = (props) => {
-    const wrappingClasses = 'relative h-10 input-component ' + (props.className || '') + (props.value ? '' : ' empty ') + (props.hasError ? ' with-error ' : '')
+    const wrappingClasses = 'relative h-10 select-component ' + (props.className || '') + (props.value ? '' : ' empty ') + (props.hasError ? ' with-error ' : '')
     const name = props.name || props.id;
 
     return (
         <div className={(props.className || '') + ' mb-2'}>
             <div className={wrappingClasses}>
-                <select value={props.value}>
+                <select value={props.value} disabled={props.readOnly} onChange={props.onChange}>
                     <option value="" disabled>Select an option</option>
-                    {props.options.map((option) => <option value={option.value}>{option.label}</option>)}
+                    {props.options.map((option) => <option value={option.value} key={option.value}>{option.label}</option>)}
                 </select>
                 <label htmlFor={name} className="absolute left-2 transition-all bg-white-100 px-1">
                     {props.label}
